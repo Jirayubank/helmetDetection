4a8eec36be86fbb4ab72c50498033ff0d8db8be7
BankkV <99808324+Jirayubank@users.noreply.github.com>
2023-11-24 14:59:02 +0700

    add docstring

--- scripts/helmetDetection.py
+++ scripts/helmetDetection.py
@@ -9,6 +9,42 @@ import paho.mqtt.client as mqtt
 
 
 class HelmetDetection:
+    """
+    Class for helmet detection using YOLO model.
+
+    Args:
+        path_model (str): Path to the YOLO model.
+        source: Image source for detection. If an integer, it represents the camera index;
+                if a string, it represents the path to an image or video file.
+        is_mqtt (bool): Flag indicating whether MQTT (Message Queuing Telemetry Transport) is enabled.
+        is_serial (bool): Flag indicating whether serial communication is enabled.
+        com_port (str, optional): COM port for serial communication if `is_serial` is True.
+
+    Attributes:
+        model: YOLO model instance for object detection.
+        isSerial (bool): Flag indicating whether serial communication is enabled.
+        isMqtt (bool): Flag indicating whether MQTT is enabled.
+        source: Image source for detection.
+        img_size (int): Size of the input image for detection.
+        color: Color for visualizations.
+        port: Serial port instance if `is_serial` is True.
+        cli: MQTT Client instance if `is_mqtt` is True.
+        zone_in: Coordinates defining the detection zone.
+        w (int): Width of the detection zone.
+        h (int): Height of the detection zone.
+
+    Methods:
+        detectionRun(): Run the helmet detection process.
+        interface(frames, detections, labels, args, fps): Display the detection interface.
+        arduino(class_id, threshold): Communicate with Arduino based on detection results.
+        mqttPaho(class_id, threshold): Publish MQTT messages based on detection results.
+        decision(class_id, threshold): Make a decision based on the average confidence of detected classes.
+
+    Note:
+        The `detectionRun` method continuously performs helmet detection and updates the interface
+        with real-time information. It also communicates with Arduino and/or publishes MQTT messages
+        based on detection results.
+    """
     def __init__(self, path_model: str, source, is_mqtt: bool, is_serial: bool, com_port=None):
         self.model = YOLO(path_model, task='detect')
         self.isSerial = is_serial
@@ -45,12 +81,6 @@ class HelmetDetection:
             self.h = 500
 
     def detectionRun(self):
-        box_annotator = sv.BoxAnnotator(
-            thickness=2,
-            text_thickness=1,
-            text_scale=0.5
-        )
-
         for results in self.model.predict(
                 source=self.source,
                 show=False,
@@ -73,7 +103,6 @@ class HelmetDetection:
                 for _, _, confidence, class_id, _
                 in detections
             ]
-
             args = numpy.ndarray([])
             if detections.class_id.size > 0:
                 args = detections.class_id
@@ -88,17 +117,27 @@ class HelmetDetection:
                     mq = threading.Thread(target=self.mqttPaho(args))
                     mq.start()
 
-            frame = box_annotator.annotate(
-                scene=frames,
+            self.interface(
+                frames=frames,
                 detections=detections,
-                labels=labels
+                labels=labels,
+                args=args,
+                fps=fps
             )
-
-            self.interface(frame, args, fps)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-    def interface(self, frame: numpy.ndarray, args: numpy.ndarray, fps: float):
+    def interface(self, frames, detections, labels, args, fps):
+        box_annotator = sv.BoxAnnotator(
+            thickness=2,
+            text_thickness=1,
+            text_scale=0.5
+        )
+        frame = box_annotator.annotate(
+            scene=frames,
+            detections=detections,
+            labels=labels
+        )
         sv.draw_polygon(frame, self.zone_in, self.color)
         if self.decision(args, 0.6) == 0:
             msg = "Please Wear Helmet!!!"
